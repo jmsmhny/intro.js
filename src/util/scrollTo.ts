a240d50f70c41754c4e309c4e3963d6cbf6d39a0
James Mahoney <jim@jamesmahoney.com>
2023-09-15 19:16:18 -0700

    h scroll support 01

--- src/util/scrollTo.ts
+++ src/util/scrollTo.ts
@@ -33,6 +33,67 @@ export default function scrollTo(
     // I have changed the scroll option and now it scrolls the window to
     // the center of the target element or tooltip.
 
+      // scroll vertically
+    if (top < 0 || targetElement.clientHeight > winHeight) {
+      window.scrollBy(
+        0,
+        rect.top - (winHeight / 2 - rect.height / 2) - scrollPadding
+      ); // 30px padding from edge to look nice
+
+      //Scroll down
+    } else {
+      window.scrollBy(
+        0,
+        rect.top - (winHeight / 2 - rect.height / 2) + scrollPadding
+      ); // 30px padding from edge to look nice
+    }
+
+      // scroll horizontally
+    const winWidth = getWindowSize().width;
+    const left = rect.right - (rect.right - rect.left);
+
+   if (left < 0 || targetElement.clientWidth > winWidth) {
+      window.scrollBy(
+          rect.left - (winWidth / 2 - rect.width / 2) - scrollPadding,
+	  0
+      ); // 30px padding from edge to look nice
+
+      // Scroll right
+    } else {
+      window.scrollBy(
+        rect.left - (winWidth / 2 - rect.width / 2) + scrollPadding,
+        0
+      ); // 30px padding from edge to look nice
+    }
+  }
+}
+
+export function scrollTo_orig(
+  scrollToElement: boolean,
+  scrollTo: ScrollTo,
+  scrollPadding: number,
+  targetElement: HTMLElement,
+  tooltipLayer: HTMLElement
+) {
+  if (scrollTo === "off") return;
+  let rect: DOMRect;
+
+  if (!scrollToElement) return;
+
+  if (scrollTo === "tooltip") {
+    rect = tooltipLayer.getBoundingClientRect();
+  } else {
+    rect = targetElement.getBoundingClientRect();
+  }
+
+  if (!elementInViewport(targetElement)) {
+    const winHeight = getWindowSize().height;
+    const top = rect.bottom - (rect.bottom - rect.top);
+
+    // TODO (afshinm): do we need scroll padding now?
+    // I have changed the scroll option and now it scrolls the window to
+    // the center of the target element or tooltip.
+
     if (top < 0 || targetElement.clientHeight > winHeight) {
       window.scrollBy(
         0,
